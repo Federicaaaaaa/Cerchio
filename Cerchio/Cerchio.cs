67aabd91ac28c4dfdf71d578105ed18b802d3fdb
Federicaaaaaa <danger2006.bona@gmail.com>
2024-01-24 19:56:30 +0100

    Aggiungere i file di progetto.

---
+++ Cerchio/Cerchio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerchio
+{
+    internal class Cerchio
+    {
+        public float Raggio { get; set; }
+        public float AreaCerchio()
+        {
+            return (float)(Raggio * Raggio * Math.PI);
+        }
+        public float Circonferenza()
+        {
+            return (float)(Raggio * 2 * Math.PI);
+        }
+        public override string ToString()
+        {
+            return string.Format("Cerchio di raggio {0}", Raggio);
+        }
+    }
+}
---
+++ Cerchio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerchio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio c = new Cerchio();
+            c.Raggio = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area: {0}", c.AreaCerchio());
+            Console.WriteLine("Circonferenza: {0}", c.Circonferenza());
+            Console.WriteLine(c.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
